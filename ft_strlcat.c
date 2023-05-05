c37e182c0bda20a2e2e3a1709182c6fc85b48e12
Oceane TOROSSIAN <octoross@e3r3p15.42.fr>
2023-05-05 18:17:33 +0200

    Wtf cette fonction

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 22:02:32 by octoross          #+#    #+#             */
-/*   Updated: 2023/05/03 22:02:32 by octoross         ###   ########.fr       */
+/*   Updated: 2023/05/05 18:17:14 by octoross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,20 @@ size_t	ft_strlcat(char *dst, char *src, size_t size)
 		dst[len + i] = '\0';
 	return (len_src + len);
 }
+
+// #include "bsd/string.h"
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc > 5)
+// 	{
+// 		char *dst = argv[1];
+// 		char *src = argv[2];
+// 		size_t return_type = ft_strlcat(dst, src, atoi(argv[3]));
+// 		printf("%zu '%s' '%s'\n", return_type, dst, src);
+// 		char *dst_ref = argv[4];
+// 		char *src_ref = argv[5];
+// 		printf("%zu '%s' '%s'\n", strlcat(dst_ref, src_ref, atoi(argv[3])), dst_ref, src_ref);
+// 	}
+// 	return (0);
+// }
