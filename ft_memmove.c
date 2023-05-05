ae5853b7a76b45f1e9b593c82b4b237537e8657a
octoross <octoross@student.42.fr>
2023-05-05 21:05:21 +0200

    Correction cast pointeur de int * a char *

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 21:30:55 by octoross          #+#    #+#             */
-/*   Updated: 2023/05/05 20:25:53 by octoross         ###   ########.fr       */
+/*   Updated: 2023/05/05 21:04:45 by octoross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	int	const	*from;
-	int			*to;
+	char const	*from;
+	char		*to;
 	size_t		i;
 
 	if (!dest || !src)
 		return (NULL);
-	from = src;
-	to = dest;
+	from = (char const *)src;
+	to = (char *)dest;
 	if (n == 0 || dest == src)
 		return (dest);
-	if (0 < to - from && to - from < (int) n)
+	if (0 < (to - from) && (to - from) < (unsigned int)n)
 	{
 		i = n;
 		while (--i >= 0)
@@ -34,7 +34,14 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	{
 		i = 0;
 		while (i++ < n)
-			to[i] = from[i];
+			to[i - 1] = from[i - 1];
 	}
 	return (dest);
 }
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc > 3)
+// 		printf("'%s'\n", (char *)ft_memmove(argv[1], argv[2], atoi(argv[3])));
+// 	return (0);
+// }
