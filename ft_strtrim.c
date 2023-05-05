8c565d63afa59d6208d49251436694762f4a59e4
Oceane TOROSSIAN <octoross@e3r3p15.42.fr>
2023-05-05 18:36:55 +0200

    Main ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 22:16:03 by octoross          #+#    #+#             */
-/*   Updated: 2023/05/04 22:16:03 by octoross         ###   ########.fr       */
+/*   Updated: 2023/05/05 18:36:35 by octoross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		start;
 	int		end;
-	int		i;
-	char	*trimmed;
 
 	if (!set || !s1)
 		return (0);
@@ -30,4 +28,17 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (end - start < 0)
 		end = start - 1;
 	return (ft_substr(s1, (unsigned int) start, (size_t)(end - start + 1)));
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 2)
+	{
+		char const *s1 = argv[1];
+		char const *set = argv[2];
+		char *return_type = ft_strtrim(s1, set);
+		printf("'%s'\n", return_type);
+		free(return_type);
+	}
+	return (0);
 }
