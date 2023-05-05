a523bd0750b9847d13af706dad51dccc0ffafb3c
octoross <octoross@student.42.fr>
2023-05-05 21:26:47 +0200

    Si src null return dest au lieu de null

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 21:29:37 by octoross          #+#    #+#             */
-/*   Updated: 2023/05/05 20:25:34 by octoross         ###   ########.fr       */
+/*   Updated: 2023/05/05 21:18:26 by octoross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t				i;
-	unsigned const char	*from;
-	unsigned char		*to;
+	size_t		i;
+	const char	*from;
+	char		*to;
 
-	if (!dest || !src)
+	if (!dest)
 		return (NULL);
+	if (!src)
+		return (dest);
 	from = src;
 	to = dest;
 	i = 0;
@@ -30,3 +32,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc > 3)
+// 		printf("'%s'\n", (char *)ft_memcpy(argv[1], argv[2], atoi(argv[3])));
+// 	return (0);
+// }
