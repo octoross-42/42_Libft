dde62ce785c5c49baaeaf464941d7117171fbb40
octoross <octoross@student.42.fr>
2023-05-08 17:27:56 +0200

    Norminage ft_putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 22:11:30 by octoross          #+#    #+#             */
-/*   Updated: 2023/05/05 22:13:59 by octoross         ###   ########.fr       */
+/*   Updated: 2023/05/08 17:27:21 by octoross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_putnbr_fd(int n, int fd)
 	long	nbr;
 
 	nbr = (long)n;
-	if (nbr  < 0)
+	if (nbr < 0)
 	{
 		ft_putchar_fd('-', fd);
 		nbr *= -1;
