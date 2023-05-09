c9e15a9d0bd6783135902bc08e6fad6a60055526
octoross <octoross@student.42.fr>
2023-05-09 14:53:51 +0200

    ft_strnstr -> Correction condition d'arret

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 22:37:56 by octoross          #+#    #+#             */
-/*   Updated: 2023/05/09 14:46:14 by octoross         ###   ########.fr       */
+/*   Updated: 2023/05/09 14:53:16 by octoross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	while (s1[i] && i < n)
 	{
 		j = 0;
-		while (i + j < n && s1[i + j] == s2[j])
+		while (i + j < n && s1[i + j] && s1[i + j] == s2[j])
 			j ++;
 		if (!s2[j])
 			return ((char *)&(s1[i]));
