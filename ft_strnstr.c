08f3ac61599f969212aa7cbc5f79d6451ccf7829
octoross <octoross@student.42.fr>
2023-05-09 14:17:36 +0200

    Correction ordre test edge case

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 22:37:56 by octoross          #+#    #+#             */
-/*   Updated: 2023/05/05 21:23:53 by octoross         ###   ########.fr       */
+/*   Updated: 2023/05/09 14:17:06 by octoross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	size_t	i;
 	size_t	j;
 
-	if (!s1[0] && !s2[0])
-		return ((char *)s1);
 	if (!s1 || !s2)
 		return (0);
+	if (!s1[0] || !s2[0])
+		return ((char *)s1);
 	if (ft_strlen(s2) > n)
 		return (0);
 	i = 0;
